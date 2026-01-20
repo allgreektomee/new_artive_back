02d9e56db5bda431319490fe6fea09a4d1095ad7
devcation <88315076+devcation-learn@users.noreply.github.com>
2026-01-20 16:48:25 +0900

    ArtworkDetailResponse

--- src/main/java/com/artivefor/me/dto/artwork/ArtworkDetailResponse.java
+++ src/main/java/com/artivefor/me/dto/artwork/ArtworkDetailResponse.java
@@ -1,30 +1,24 @@
 package com.artivefor.me.dto.artwork;
 
+import com.artivefor.me.data.artwork.WorkStatus;
 import com.artivefor.me.data.common.Visibility;
 import lombok.*;
 
 import java.util.List;
 
-@Getter
-@Setter
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class ArtworkDetailResponse {
-    private Long id;
-    private String thumbnailUrl;
-    private String medium;
-    private String size;
-    private Visibility visibility; // PUBLIC, PRIVATE
-    private List<String> images;
-
-    // 프론트엔드 ArtworkFormValues와 매칭되는 필드
-    private String koTitle;
-    private String koDescription;
-    private String enTitle;
-    private String enDescription;
-
-    // 필요한 경우 날짜 필드 추가
-    private String startedAt;
-    private String finishedAt;
-}
+public record ArtworkDetailResponse(
+    Long id,
+    String koTitle,
+    String koDescription,
+    String enTitle,
+    String enDescription,
+    String thumbnailUrl,
+    List<String> images,
+    Visibility visibility,
+    String medium,
+    String size,
+    String startedAt,
+    String finishedAt,
+    WorkStatus status
+) {}
