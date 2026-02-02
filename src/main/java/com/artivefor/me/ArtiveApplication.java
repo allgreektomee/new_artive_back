30d327c120b9e2a3933b1e6ba7b185d22fb98754
devcation <88315076+devcation-learn@users.noreply.github.com>
2026-02-02 11:14:11 +0900

    history type content

--- src/main/java/com/artivefor/me/ArtiveApplication.java
+++ src/main/java/com/artivefor/me/ArtiveApplication.java
@@ -9,8 +9,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ArtiveApplication {
 
