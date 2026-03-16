191246681b47aef25152f2e9973e3a87ec8a1380
devcation <88315076+devcation-learn@users.noreply.github.com>
2026-03-16 19:55:47 +0900

    admin

--- src/main/java/com/artivefor/me/repository/content/ArticleRepository.java
+++ src/main/java/com/artivefor/me/repository/content/ArticleRepository.java
@@ -11,5 +11,6 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
     @EntityGraph(attributePaths = {"category"})
     Page<Article> findAllByType(ContentType type, Pageable pageable);
 
+    @EntityGraph(attributePaths = {"category"})
     Page<Article> findAllByTypeAndCategoryId(ContentType type, Long categoryId, Pageable pageable);
 }
